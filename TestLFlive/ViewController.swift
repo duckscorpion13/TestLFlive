eacbaea415b98adc515076e55974aac5b8493a6d
derekyang <derek.y@tc-gaming.com>
2020-12-03 17:13:11 +0800

    Initial Commit

---
+++ TestLFlive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestLFlive
+//
+//  Created by derekyang on 2020/12/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
